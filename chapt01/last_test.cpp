5fef465168805d0d56f8122a7067387d45721925
Allen Woods <allen.woods@foxmail.com>
2015-08-31 22:39:09 +0800

    Finish chapt01

--- chapt01/last_test.cpp
+++ chapt01/last_test.cpp
@@ -7,5 +7,29 @@
 
 int main()
 {
-
+    Sales_item currentBook;
+    if (std::cin >> currentBook)  //Make Sure there is Input (from file)
+    {
+        Sales_item transData;
+        while (std::cin >> transData) //Read data from file
+        {
+            if(currentBook.isbn() == transData.isbn()) //If they are the same book
+            {
+                currentBook += transData; //Add the sales (defined by the class)
+            }
+            else
+            {
+                std::cout << currentBook << std::endl;  // Print the sales of
+                                                        // last book
+                currentBook = transData;
+            }
+        }
+        std::cout << currentBook << std::endl; //Print the last book
+    }
+    else
+    {
+        std::cerr << "No Data Found!" << std::endl; //Warning: there is no Input
+        return -1;
+    }
+    return 0;
 }
